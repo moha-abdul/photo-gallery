446940bb7e5f85621bcc16d804c3f0e0edd2becd
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-09-28 11:51:00 +0300

    Add category addition to site

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Photos
+from .models import Photos, Category
 
 admin.site.register(Photos)
+admin.site.register(Category)
--- gallery/models.py
+++ gallery/models.py
@@ -1,10 +1,31 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+class Location(models.Model):
+    name = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.name
+    
+    def save_location(self):
+        self.save()
+
+
 class Photos(models.Model):
     photo = models.ImageField(upload_to='myphotos/')
     name = models.CharField(max_length =60)
     description = models.TextField()
-    category = models.ForeignKey(Category)
+    category = models.ForeignKey(Category, default=True)
+    location = models.ForeignKey(Location, default=True)
 
     def save_image(self):
         self.save()
@@ -23,11 +44,4 @@ class Photos(models.Model):
     #     photos = Photos.objects.filter()
     #     return photos
 
-class Category(models.Model):
-    name = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.name
-
-    def save_category(self):
-        self.save()
