2745119fb0caaee932a856fd2cea7a40d3beceda
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-09-28 12:02:30 +0300

    Add location model

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Photos, Category
+from .models import Photos, Category, Location
 
 admin.site.register(Photos)
 admin.site.register(Category)
+admin.site.register(Location)
