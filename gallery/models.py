d28d3fbb8d2a105a4100722b7c72206d0cca7ee9
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-09-28 10:46:31 +0300

    add category model

--- gallery/models.py
+++ gallery/models.py
@@ -4,12 +4,16 @@ class Photos(models.Model):
     photo = models.ImageField(upload_to='myphotos/')
     name = models.CharField(max_length =60)
     description = models.TextField()
+    category = models.ForeignKey(Category)
 
     def save_image(self):
         self.save()
 
     def delete_image(self):
         self.delete()
+
+    def update_image(self):
+        self.update_image()
 
     def __str__(self):
         return self.name
@@ -18,3 +22,12 @@ class Photos(models.Model):
     # def all_photos(cls):
     #     photos = Photos.objects.filter()
     #     return photos
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
